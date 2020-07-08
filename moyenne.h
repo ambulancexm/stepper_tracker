9a7f21078a304a75c7076aebc7aa1078fafd4f65
Dydouch <dylan.turnier@dartbox.com>
2020-07-08 10:26:33 +0200

    ajout moyenne

---
+++ moyenne.h
@@ -0,0 +1,14 @@
+#include <Stepper.h>
+#define BOUCLE 10
+
+int moyenne (int pin)
+{
+    int i = 0;
+    int moy = 0;
+    while (i <= BOUCLE) {
+        moy += analogread(pin);
+        i++;
+    }
+
+    return moy/BOUCLE;
+}
