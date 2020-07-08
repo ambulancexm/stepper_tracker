f26e57ef69976440bd7497908374e3c82cbb700b
bulleux <bereziat.th@gmail.com>
2020-07-08 10:39:26 +0200

    modif step revolutiuon

--- configStepper.h
+++ configStepper.h
@@ -1,18 +1,14 @@
 #define INTERVAL 10
 #include <Stepper.h>
 
-const int stepsPerRevolution = 500;
+const int stepsPerRevolution = 360;
 
 void moveStepper(int ref1, int ref2, int interval, Stepper stepperMotor){
   int delta = (analogRead(ORANGE)-analogRead(BLEU));
   if (delta > interval){
-  int stp = stepsPerRevolution;
+  int stp = stepsPerRevolution/45;
   stepperMotor.setSpeed(motorSpeed);
-  if (delta <5){
-    stp = stp/16;
-  }else{
-    stp =stp /8;
-  }
+ 
   stepperMotor.step(stp);
 }
 }
